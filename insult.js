54e906f29ed789c1a2a1bc24003d1bb00129b60a
Chris Burgdorff <wesborland1234@aol.com>
2022-05-05 00:34:16 -0400

    Updated with some new insults
    Final probably

--- insult.js
+++ insult.js
@@ -1,12 +1,21 @@
-
-
 function generateInsult() {
-  const adjectives = ["shitty", "fucked", "braindead", "cumguzzling"];
-  const nouns = ["dingus", "fuckwad", "cockgobbler", "waste of carbon"];
+  const adjectives = ["shitty", "braindead", "cumguzzling", "junky", "cunty", "taint-tickling", "worthless", "syphillis laden", "short bus riding", "clown school reject ass", "inconsequential", "skidmark sniffing", "chode chomping", "bitch tit having", "sperm burping", "slunty"];
+  const nouns = ["dingus", "fuckwad", "cockgobbler", "waste of carbon", "window licker", "vaginal spewage", "poster child for abortion", "fuckface", "ball cradler", "grundle", "pap smear", "thundercunt", "ass gape", "fuck ass", "fruity ass", "skeet burrito", "slut muffin", "twat monger", "semen magnet", "street walker", "genetic tragedy", "cunt basket", "saggy vulva", "cock garage"]; 
   const selectedAdjective = adjectives[Math.floor(Math.random()*adjectives.length)];
   const selectedNoun = nouns[Math.floor(Math.random()*nouns.length)];
   const insult = `You are a ${selectedAdjective} ${selectedNoun}!`;
   return insult;
 }
 
-alert(generateInsult());
+console.log(generateInsult());
+
+
+
+
+
+
+
+
+
+
+
